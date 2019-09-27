6555430e7bbe5835c1b1bf23501090947d848abb
Roy Nyaga <nyagaandreroy@gmail.com>
2019-09-27 19:48:12 +0100

    fixed rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-	validates :username,  presence: true, length: { maximum: 20 }
-	validates :email, presence: true 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, length: { maximum: 255 },
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 end
